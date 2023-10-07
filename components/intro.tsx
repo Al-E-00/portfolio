7cfaaf418bd442efa015ecdbafa0f719265c24f5
Alessio <alessio.mazzella@siren.io>
2023-10-07 15:06:17 +0200

    Added buttons in intro section - Contact me - Download CV - Linkedin - GitHub

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -39,6 +43,44 @@ export default function Intro() {
         solutions. Dive into my portfolio and see how we can elevate{" "}
         <span className="font-bold">your</span> next project.
       </motion.p>
+
+      <motion.div
+        className="flex flex-col justify-center gap-3 px-2 text-lg font-medium sm:flex-row"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group flex items-center gap-2 rounded-full bg-gray-900 px-7 py-3 text-white outline-none transition hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-105"
+        >
+          Contact me{" "}
+          <BsArrowRight className="opacity-70 transition group-hover:translate-x-1" />
+        </Link>
+
+        <a
+          href="/alessio_mazzella_cv.pdf"
+          download
+          className="borderBlack group flex cursor-pointer items-center gap-2 rounded-full bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 transition group-hover:translate-y-1" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/alessio-mazzella/?profileId=ACoAADUol8MB1XTW0hPIvPcD9mwzm8J_bbfah6A"
+          target="_blank"
+          className="borderBlack flex cursor-pointer items-center gap-2 rounded-full bg-white p-4 text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/Al-E-00"
+          target="_blank"
+          className="borderBlack flex cursor-pointer items-center gap-2 rounded-full bg-white p-4 text-[1.35rem] text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
