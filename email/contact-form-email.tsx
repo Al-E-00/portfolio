5df1c853a5b05e445b7b7b3ca7590f3153fa2dfe
Alessio <alessio.mazzella@siren.io>
2023-10-10 08:22:54 +0200

    - Added react-email: - Downgraded to version 0.0.8 because of a bug - Added styles to the email

---
+++ email/contact-form-email.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Head,
+  Heading,
+  Container,
+  Hr,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+
+//Downgraded to version 0.0.8
+import { Tailwind } from "@react-email/tailwind";
+
+type ContactFormEmailProps = {
+  message: string;
+  senderEmail: string;
+};
+
+export default function ContactFormEmail({
+  message,
+  senderEmail,
+}: ContactFormEmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>New message from your portfolio site</Preview>
+      <Tailwind>
+        <Body className="rounded-md bg-gray-100 px-4 text-gray-800">
+          <Container>
+            <Section className="borderBlack my-10 rounded-md bg-white py-4">
+              <Heading className="leading-tight">
+                You received the following message from the contact form
+              </Heading>
+              <Text>{message}</Text>
+              <Hr />
+              <Text>The sender's email is: {senderEmail}</Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
